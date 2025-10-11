00361829215fe2cf2e26ab89e869778c7d992c41
Nenzui_laptop <yoshida8saikou@gmail.com>
2025-10-11 22:55:09 +0900

    鍵盤の座標と色のしきい値設定完了。次はmidi化

--- main.py
+++ main.py
@@ -43,11 +43,11 @@ class App:
 
         #鍵盤の位置を設定するウィンドウを開くボタン
         self.set_keyboard_position_button = tk.Button(text="鍵盤の位置を指定", command=self.open_Setting_position)
-        self.set_keyboard_position_button.place(x=10, y=50)
+        self.set_keyboard_position_button.place(x=10, y=90)
 
         #動画から画像に変換ボタン
         self.start_button = tk.Button(text="動画を画像に変換", command=self.video_to_image)
-        self.start_button.place(x=10, y=90)
+        self.start_button.place(x=10, y=50)
 
 
 
@@ -125,6 +125,24 @@ class App:
         messagebox.showinfo("infomation", "変換が正常に終了しました")
 
 
+    def detect_notes(self):
+        if not self.video_info:
+            try: #動画情報がないときに、情報を取得する
+                self.video_info = ffmpeg.probe(self.filename)
+                self.start_button["text"] = "動画を画像に変換"
+                self.start_button["state"] = "normal"
+                self.width = self.video_info["streams"][0]["width"]
+                self.height = self.video_info["streams"][0]["height"]
+                self.fps = self.video_info["streams"][0]["avg_frame_rate"]
+                self.avg_fps = float(Fraction(self.fps)) #平均のfpsを計算
+            except:
+                #エラー時はポップアップし、ボタンを戻す
+                messagebox.showerror("Error", "ファイルが選択されていません")
+
+        
+
+        
+
     def open_Setting_position(self):
         # 既に開いている場合はフォーカス
         if hasattr(self, "setting_win") and self.setting_win.winfo_exists():
@@ -139,6 +157,7 @@ class App:
 class Setting_position(tk.Toplevel):
     def __init__(self, master):
         super().__init__(master)
+        self.app  = master #Appのインスタンスを保存
         #ウィンドウ設定
         self.title("Setting_position")
         self.geometry("1000x700")
@@ -148,6 +167,9 @@ class Setting_position(tk.Toplevel):
 
         #鍵盤の座標を保存する辞書
         self.key_positions = {}
+
+        #オクターブの距離
+        self.octave_offset_x = 0
 
         #キャンバスの作成
         self.canvas_height=450
@@ -197,18 +219,31 @@ class Setting_position(tk.Toplevel):
         color_info_label5.place(x=160, y=470)
         color_info_label6.place(x=185, y=470)
 
+        info_label = tk.Label(self, text="すべての鍵盤が押されてない状態にしてください", font=12)
+        info_label.place(x=20, y=550)
+
         #座標を指定するウィジェットを追加
         self.C4_box = DraggableRectangle(self.canvas, 20, 20, 7, 15, "red")
         self.C4_Sharp_box = DraggableRectangle(self.canvas, 30, 20, 6, 15, "green")
         self.B4_box = DraggableRectangle(self.canvas, 40, 20, 7, 15, "blue")
 
         #自動で鍵盤の座標を補充するボタン
-        self.add_key = tk.Button(self, text="鍵盤を自動追加", command=self.add_keys)
+        self.add_key = tk.Button(self, text="鍵盤を自動追加", command=self.add_all_octaves)
         self.add_key.place(x=20, y=580)
+
+        #鍵盤の色のしきい値を取得するボタン
+        self.set_threshold_button = tk.Button(self, text="色のしきい値を取得", command=self.set_threshold)
+        self.set_threshold_button.place(x=120, y=580)
+
+        #座標としきい値の色を確定するボタン
+        self.confirm_positions_button = tk.Button(self, text="座標を確定", command=self.apply_and_close, bg="coral")
+        self.confirm_positions_button.place(x=20, y=620)
     
 
     def resize_image(self, image_path):
         try:
+            self.image_path = image_path
+
             pil_image = Image.open(image_path)
             image_width, image_height = pil_image.size
 
@@ -216,11 +251,11 @@ class Setting_position(tk.Toplevel):
             width_ratio = self.canvas_width / image_width
             height_ratio = self.canvas_height / image_height
             #小さいほうの縮小率を使用
-            ratio = min(width_ratio, height_ratio)
+            self.ratio = min(width_ratio, height_ratio)
 
             #画像の縮小後のサイズを計算
-            new_width = int(image_width * ratio)
-            new_height = int(image_height * ratio)
+            new_width = int(image_width * self.ratio)
+            new_height = int(image_height * self.ratio)
 
             #Pillowで画像をリサイズ
             resized_pil_image = pil_image.resize((new_width, new_height), Image.Resampling.LANCZOS)
@@ -249,56 +284,129 @@ class Setting_position(tk.Toplevel):
         self.canvas.lower(self.image_id)
 
 
-    def add_keys(self):
+    def add_all_octaves(self):
+        #移動可能なブロックのクラスを保存する辞書
+        self.all_dragable_keys = {}
+
+        #ブロックの座標を保存する辞書を初期化
+        self.key_positions = {}
+
         #最初にユーザーが指定した座標ブロックをcanvasから隠す
         if self.C4_box:
             self.canvas.itemconfig(self.C4_box.item, state="hidden")
             self.canvas.itemconfig(self.C4_Sharp_box.item, state="hidden")
             self.canvas.itemconfigure(self.B4_box.item, state="hidden")
 
+        #初回のC4の実行
+        self.add_keys(octave=4)
+
+        #他の生成したいオクターブのリストを作成
+        other_octave = [1, 2, 3, 5, 6, 7]
+        for oct in other_octave:
+            self.add_keys(octave=oct)
+
+
+    def add_keys(self, octave):
         C_x, C_y = self.C4_box.get_position()
         _, C_Sharp_y = self.C4_Sharp_box.get_position()
         B_x, _ = self.B4_box.get_position()
-        
-        octave=4
+        canvas_width = self.canvas.winfo_width()
+
         keys=["C", "C_Sharp", "D", "D_Sharp", "E", "E_None", "F", "F_Sharp", "G", "G_Sharp", "A", "A_Sharp", "B"]
         t = len(keys) - 1 #鍵盤の数
 
-        #推定した鍵盤のクラスを保存する辞書
-        self.all_dragable_keys = {}
+        #C4を基準(0)として、左右に何オクターブずれるか計算
+        base_C4_offset_x = octave - 4
 
-        #鍵盤の座標を計算
+        #C4からB4の距離を計算, CとD間の距離を計算
+        if not octave==4:
+            C4_x, _ = self.all_dragable_keys["C_4"].get_position()
+            C4_Sharp_x, _ = self.all_dragable_keys["C_Sharp_4"].get_position()
+            B4_x, _ = self.all_dragable_keys["B_4"].get_position()
+            self.octave_offset_x = B4_x - C4_x
+            
+            self.note_distance_x = C4_Sharp_x - C4_x
+
+            #11音に1音追加して12音分の距離にする
+            print(f"{self.octave_offset_x + (self.note_distance_x * 2)} = {self.octave_offset_x} + {self.note_distance_x * 2}")
+            self.octave_offset_x = self.octave_offset_x + (self.note_distance_x * 2)
+
+        #オクターブのずれを計算
+        octave_distance = self.octave_offset_x * base_C4_offset_x
+
+        #1オクターブ分の鍵盤の座標を計算
         for i, note in enumerate(keys):
             note = note + "_" + str(octave)
             x = (1 - (i / t)) * C_x + ((i / t) * B_x) #線形補完で座標を計算
 
+            #オクターブごとに座標をずらす
+            x = x + octave_distance
+
             #Eの半音上はないのでスキップ
             if note == f"E_None_{octave}":
-                pass
+                continue
+
             #シャープはy座標が違うため分岐
-            elif "_Sharp" in note:
-                position = [x, C_Sharp_y]
-                self.key_positions[note] = position
-
-                #辞書に保存
-                x1_s = x - (6 / 2) #center_x - (width / 2)
-                y1_s = C_Sharp_y - (15 / 2) #center_y - (height / 2)
-                self.all_dragable_keys[f"{note}_{octave}"] = DraggableRectangle(self.canvas, x1_s, y1_s, 6, 15, "gray")
-
-
-            else: #白鍵の場合の処理
-                position = [x, C_y]
-                self.key_positions[note] = position
-
-                #辞書に保存
-                x1 = x - (7 / 2) #center_x - (width / 2)
-                y1 = C_y - (15 / 2)
-                self.all_dragable_keys[f"{note}_{octave}"] = DraggableRectangle(self.canvas, x1, y1, 7, 15, "gray")
+            if "_Sharp" in note:
+                w, h = 6, 15
+                center_y = C_Sharp_y
+                x1 = x - w / 2
+                y1 = center_y - h / 2
+                x2 = x1 + w
+                # 範囲内のみ追加
+                if 0 <= x1 and x2 <= canvas_width:
+                    self.key_positions[note] = {"position": [x / self.ratio, center_y / self.ratio], "color": None} #縮小前の画像の座標にして辞書に追加
+                    self.all_dragable_keys[note] = DraggableRectangle(self.canvas, x1, y1, w, h, "gray")
+                else:
+                    # クリーンアップ
+                    self.key_positions.pop(note, None)
+                    if note in self.all_dragable_keys:
+                        self.canvas.delete(self.all_dragable_keys[note].item)
+                        self.all_dragable_keys.pop(note, None)
+            #白鍵の時の処理
+            else:
+                w, h = 7, 15
+                center_y = C_y
+                x1 = x - w / 2
+                y1 = center_y - h / 2
+                x2 = x1 + w
+                if 0 <= x1 and x2 <= canvas_width:
+                    self.key_positions[note] = {"position": [x / self.ratio, center_y / self.ratio], "color": None} #縮小前の画像の座標にして辞書に追加
+                    self.all_dragable_keys[note] = DraggableRectangle(self.canvas, x1, y1, w, h, "gray")
+                else:
+                    self.key_positions.pop(note, None)
+                    if note in self.all_dragable_keys:
+                        self.canvas.delete(self.all_dragable_keys[note].item)
+                        self.all_dragable_keys.pop(note, None)
 
         print(self.key_positions)
 
 
+    def set_threshold(self):
+        if not self.key_positions:
+            messagebox.showerror("error", "座標を指定してください")
+        else:
+            for key in self.key_positions:
+                img=Image.open(self.image_path)
+                #画像をRGBモードに変換
+                rgb_img = img.convert("RGB")
+                #指定した座標の色を取得
+                x = self.key_positions[key]["position"][0]
+                y = self.key_positions[key]["position"][1]
+                r, g, b = rgb_img.getpixel((x, y))
+                #辞書にリスト型で色を追加 [r, g, b]
+                self.key_positions[key]["color"] = [r, g, b]
+                print(f"{key}の色は{r, g, b}")
+ 
 
+    def apply_and_close(self):
+        if not self.key_positions:
+            messagebox.showerror("error", "座標を指定してください")
+        else:
+            self.app.key_positions = self.key_positions #Appに辞書を渡す
+            print(f"座標が確定されました: {self.app.key_positions}")
+            self.destroy()
+        
 
 class DraggableRectangle:
     def __init__(self, canvas, x, y, width, height, color):
@@ -333,7 +441,7 @@ class DraggableRectangle:
         self.start_x = event.x
         self.start_y = event.y
 
-    def get_position(self):
+    def get_position(self): #ブロックの中心の座標を返すメソッド
         position = self.canvas.coords(self.item)
         #ブロックの中央の座標を計算
         x = (position[0] + position[2]) / 2
